b0700e50b9225ef55e1a8225317551b17e34f59c
Kimeunseong <eskim1717@gmail.com>
2022-10-20 22:32:06 +0900

    221020

--- week02/숫자 카드2.py
+++ week02/숫자 카드2.py
@@ -1,9 +1,9 @@
 # 10816번 : 숫자 카드2
 
 n = int(input())
-cards = [int(input()) for i in range(n)]
+cards = [int(input()) for i in range(n)] # n개의 숫자 카드를 입력받는다.
 for i in range(int(input())):
-    card = int(input())
-    print(cards.count(card))
+    card = int(input())                   # 숫자를 입력받아,
+    print(cards.count(card))                # 해당 숫자가 몇 개 있는지 확인후 출력한다.
 
 # 미완성 코드ㅠㅜㅠ 입력 방식이 잘못됐음.. 다시 해보쟈..
